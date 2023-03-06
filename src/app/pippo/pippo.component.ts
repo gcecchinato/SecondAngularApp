ef1da41f0db968aeddb1b2af7e5b4a15bb4063fe
gcecchinato <giacomocecchinato@libero.it>
2023-03-06 02:28:47 +0100

    Buttons work

--- src/app/pippo/pippo.component.ts
+++ src/app/pippo/pippo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pippo',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class PippoComponent {
 
+  clickMessage = '';
+
+  onClickDer() {
+    this.clickMessage = 'Der';
+  }
+  onClickDie() {
+    this.clickMessage = 'Die';
+  }
+  onClickDas() {
+    this.clickMessage = 'Das';
+  }
 }
