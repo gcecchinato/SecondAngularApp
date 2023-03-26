0f9c5dae0ae5a34f76f4b377c18ad4416034e4f6
gcecchinato <giacomocecchinato@libero.it>
2023-03-26 23:58:27 +0200

    json reading works

--- src/app/pippo/pippo.component.ts
+++ src/app/pippo/pippo.component.ts
@@ -1,21 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+
+import jsontest from './jsontest.json';
+import woerter from './woerter.txt';
+
+
 
 @Component({
   selector: 'app-pippo',
   templateUrl: './pippo.component.html',
   styleUrls: ['./pippo.component.css']
 })
-export class PippoComponent {
-
+export class PippoComponent{
   clickMessage = '';
+    title = 'json-file-read-angular';
+
+  // public countryList:{name:string, code:string}[] = woerter;
 
   onClickDer() {
     this.clickMessage = 'Der';
+    // this.clickMessage = jsontest;
+
+    // this.readFile();
   }
   onClickDie() {
     this.clickMessage = 'Die';
+    this.clickMessage = jsontest;
   }
   onClickDas() {
     this.clickMessage = 'Das';
+  }
+
+
+  readFile()
+  {
+    // this.httpClient
+    // .get('assets/woerter.txt', { responseType: 'text' })
+    // .subscribe((data) => {
+    //   console.log(data);
+    // });
+
   }
 }
