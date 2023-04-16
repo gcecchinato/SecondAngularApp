96c9ba7b852603e7ebc892cb2c92dd525adedcdf
gcecchinato <giacomocecchinato@libero.it>
2023-04-16 17:51:21 +0200

    showing json content

--- src/app/pippo/pippo.component.ts
+++ src/app/pippo/pippo.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import jsontest from './jsontest.json';
+import countries from './countries.json';
 import woerter from './woerter.txt';
 
 
@@ -13,18 +13,17 @@ import woerter from './woerter.txt';
 export class PippoComponent{
   clickMessage = '';
     title = 'json-file-read-angular';
+    public countryList:{name:string, code:string}[] = countries;
 
   // public countryList:{name:string, code:string}[] = woerter;
 
   onClickDer() {
     this.clickMessage = 'Der';
-    // this.clickMessage = jsontest;
-
-    // this.readFile();
   }
   onClickDie() {
     this.clickMessage = 'Die';
-    this.clickMessage = jsontest;
+    this.clickMessage = this.countryList[0].name.toString();
+
   }
   onClickDas() {
     this.clickMessage = 'Das';
