a003257dfdac20c9bcfc9052c2689350b3857d45
gcecchinato <giacomocecchinato@libero.it>
2023-05-21 23:25:40 +0200

    Changed project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SecondAngularApp
+# der-die-das
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.6.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SecondAngularApp'`, () => {
+  it(`should have as title 'der-die-das'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SecondAngularApp');
+    expect(app.title).toEqual('der-die-das');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SecondAngularApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('der-die-das app is running!');
   });
 });
